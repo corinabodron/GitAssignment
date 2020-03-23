f01db6b7c0feedf2a3d8154576688221e3d2bfe6
Corina <corinabodron@yahoo.com>
2020-03-23 20:53:05 +0200

    commit 1

--- Tema2/src/TestGit.java
+++ Tema2/src/TestGit.java
@@ -5,8 +5,7 @@ public class TestGit {
 		System.out.println("Hello Git ! Denumirea proiectului de licenta este "
 				+ "Solutii informatice pentru analiza datelor in domeniul vanzarii");
       
-		Produs produs= new Produs("Aspirator201",45);
-		System.out.println(produs.getNumarProduse());
+		
 	}
 
 }
